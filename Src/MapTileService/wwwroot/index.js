1527b4d31dbc0b75f96aa9c5a50faeba8530add5
Andrey <andrey-vat@yandex.ru>
2020-08-02 18:11:07 +0300

    Updated Leaflet to latest version (1.6.0); minor js code cleanup

--- Src/MapTileService/wwwroot/index.js
+++ Src/MapTileService/wwwroot/index.js
@@ -1,64 +1,73 @@
-﻿// TODO: get tilesets list from TMS capabilities
-var baseMaps = {
-    'world': L.tileLayer('/tms/1.0.0/world/{z}/{x}/{y}.png', {
-        attribution: 'Esri.com',
-        maxZoom: 5,
-        tms: true
-    }),
-    'world-fs': L.tileLayer('/tms/1.0.0/world-fs/{z}/{x}/{y}.png', {
-        attribution: 'Esri.com',
-        maxZoom: 5,
-        tms: true
-    }),
-    'digitalglobe (TMS)': L.tileLayer('/tms/1.0.0/digitalglobe/{z}/{x}/{y}.jpg', {
-        attribution: '',
-        maxZoom: 17,
-        tms: true
-    }),
-    'digitalglobe (not TMS)': L.tileLayer('/api/tiles/?tileset=digitalglobe&x={x}&y={y}&z={z}', {
-        attribution: '',
-        maxZoom: 17
-    }),
-    'world-countries': L.tileLayer('/tms/1.0.0/world-countries/{z}/{x}/{y}.png', {
-        attribution: 'Esri.com',
-        maxZoom: 5,
-        tms: true
-    }),
-    'digitalglobe-cache': L.tileLayer('/tms/1.0.0/digitalglobe-cache/{z}/{x}/{y}.jpg', {
-        attribution: '',
-        maxZoom: 17,
-        tms: true
-    }),
-    'RPi.world': L.tileLayer('/tms/1.0.0/world-rpi/{z}/{x}/{y}.png', {
-        attribution: 'Esri.com',
-        maxZoom: 5,
-        tms: true
-    }),
-    'RPi.world-fs': L.tileLayer('/tms/1.0.0/world-fs-rpi/{z}/{x}/{y}.png', {
-        attribution: 'Esri.com',
-        maxZoom: 5,
-        tms: true
-    }),
-    'RPi.digitalglobe (not TMS)': L.tileLayer('/api/tiles/?tileset=digitalglobe-rpi&x={x}&y={y}&z={z}', {
-        attribution: '',
-        maxZoom: 17
-    })
-};
+﻿(function () {
 
-var tileGrid = L.gridLayer.tileGrid({
-    opacity: 1.0,
-    zIndex: 2,
-    pane: 'overlayPane'
-});
+    // TODO: update Tile Grid overlay after base map change
+    // TODO: get tilesets list from TMS capabilities response
 
-var overlayMaps = {
-    "Tile Grid": tileGrid
-};
+    var baseMaps = {
+        'world': L.tileLayer('/tms/1.0.0/world/{z}/{x}/{y}.png', {
+            attribution: 'Esri.com',
+            maxZoom: 5,
+            tms: true
+        }),
+        'world-fs': L.tileLayer('/tms/1.0.0/world-fs/{z}/{x}/{y}.png', {
+            attribution: 'Esri.com',
+            maxZoom: 5,
+            tms: true
+        }),
+        'digitalglobe (TMS)': L.tileLayer('/tms/1.0.0/digitalglobe/{z}/{x}/{y}.jpg', {
+            attribution: '',
+            maxZoom: 17,
+            tms: true
+        }),
+        'digitalglobe (not TMS)': L.tileLayer('/api/tiles/?tileset=digitalglobe&x={x}&y={y}&z={z}', {
+            attribution: '',
+            maxZoom: 17
+        }),
+        'world-countries': L.tileLayer('/tms/1.0.0/world-countries/{z}/{x}/{y}.png', {
+            attribution: 'Esri.com',
+            maxZoom: 5,
+            tms: true
+        }),
+        'digitalglobe-cache': L.tileLayer('/tms/1.0.0/digitalglobe-cache/{z}/{x}/{y}.jpg', {
+            attribution: '',
+            maxZoom: 17,
+            tms: true
+        }),
+        'RPi.world': L.tileLayer('/tms/1.0.0/world-rpi/{z}/{x}/{y}.png', {
+            attribution: 'Esri.com',
+            maxZoom: 5,
+            tms: true
+        }),
+        'RPi.world-fs': L.tileLayer('/tms/1.0.0/world-fs-rpi/{z}/{x}/{y}.png', {
+            attribution: 'Esri.com',
+            maxZoom: 5,
+            tms: true
+        }),
+        'RPi.digitalglobe (not TMS)': L.tileLayer('/api/tiles/?tileset=digitalglobe-rpi&x={x}&y={y}&z={z}', {
+            attribution: '',
+            maxZoom: 17
+        })
+    };
 
-var map = L.map('map', {
-    inertia: false,
-    doubleClickZoom: false,
-    layers: [baseMaps['world']]
-}).setView([0, 0], 0);
-// TODO: update Tile Grid overlay after base map change
-L.control.layers(baseMaps, overlayMaps).addTo(map);
+    var tileGrid = L.gridLayer.tileGrid({
+        opacity: 1.0,
+        zIndex: 2,
+        pane: 'overlayPane'
+    });
+
+    var overlayMaps = {
+        'Tile Grid': tileGrid
+    };
+
+    var map = L.map('map', {
+        inertia: false,
+        doubleClickZoom: false,
+        layers: [baseMaps['world']]
+    }).setView([0, 0], 0);
+
+
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(map);
+
+})();
